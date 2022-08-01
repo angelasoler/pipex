d3eaecceb6ca754365a699728c7a6d9f7c415636
asoler <asoler@student.42sp.org.br>
2022-08-01 22:03:50 +0200

    exit code 127 when is an non existing command

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 21:14:54 by asoler            #+#    #+#             */
-/*   Updated: 2022/08/01 21:30:02 by asoler           ###   ########.fr       */
+/*   Updated: 2022/08/01 21:56:13 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,13 @@ int	fork_cmd1(t_args *args)
 	args->file_fd = open(args->argv[1], O_RDONLY);
 	args->cmd1 = ft_split(args->argv[2], ' ');
 	args->cmd1[0] = ft_strjoin("/usr/bin/", args->cmd1[0]);
+	if (!verify_access(args->cmd1[0], F_OK))
+	{
+		args->proc.pid_in = -1;
+		free_array(args->cmd1);
+		args->proc.ret = 127;
+		return (127);
+	}
 	args->proc.pid_in = fork();
 	if (args->proc.pid_in < 0)
 		return (1);
@@ -34,10 +41,18 @@ int	fork_cmd2(t_args *args)
 	if (!verify_access(args->argv[4], W_OK))
 	{
 		args->proc.pid_out = -1;
+		args->proc.ret = 1;
 		return (1);
 	}
 	args->cmd2 = ft_split(args->argv[3], ' ');
 	args->cmd2[0] = ft_strjoin("/usr/bin/", args->cmd2[0]);
+	if (!verify_access(args->cmd2[0], F_OK))
+	{
+		args->proc.pid_out = -1;
+		free_array(args->cmd2);
+		args->proc.ret = 127;
+		return (127);
+	}
 	args->proc.pid_out = fork();
 	if (args->proc.pid_out < 0)
 		return (1);
@@ -70,8 +85,8 @@ int	handle_processes(t_args *args)
 	if (args->proc.pid_in != -1)
 	{
 		wait_and_free(args->proc.pid_in, args->cmd1, &args->proc.status);
-		WIFEXITED(args->proc.status);
-		args->proc.ret = WIFEXITED(args->proc.status);
+		if (WIFEXITED(args->proc.status))
+			args->proc.ret = WEXITSTATUS(args->proc.status);
 	}
 	dup2(args->proc.pipe_fd[0], 0);
 	close(args->proc.pipe_fd[0]);
